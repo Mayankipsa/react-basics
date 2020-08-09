7cd5c7d0311de5ebe5df9f1de1cd75913efdc8d3
Mayank <mayankipsa@gmail.com>
2020-08-09 20:53:13 +0530

    Updates

---
+++ precache-manifest.9e46811b103340e4470f84b542a0ed76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5594cb68a28161b013d946b4fe9792",
+    "url": "/react-basics/index.html"
+  },
+  {
+    "revision": "91cafbc2e307512decf9",
+    "url": "/react-basics/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d415706b3a541f83c963",
+    "url": "/react-basics/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "91cafbc2e307512decf9",
+    "url": "/react-basics/static/js/2.cdd576ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics/static/js/2.cdd576ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d415706b3a541f83c963",
+    "url": "/react-basics/static/js/main.3a92253a.chunk.js"
+  },
+  {
+    "revision": "ecb067e5745a49c636d5",
+    "url": "/react-basics/static/js/runtime-main.74e35950.js"
+  }
+]);
