9c21a7432cf386346d0ef2c71a411d0d095c218f
Mayank <mayankipsa@gmail.com>
2020-08-09 12:34:39 +0530

    Updated the Index file to load the component.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello, React Again!</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
