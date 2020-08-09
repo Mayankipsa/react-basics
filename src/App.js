035dbaed4b09dc46e491ba035be51e66cad4f1e9
Mayank <mayankipsa@gmail.com>
2020-08-09 13:51:34 +0530

    Loaded Table in App Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Table from './Table'
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <h1>Hello, React Again!</h1>
+                <Table></Table>
             </div>
         )
     }
