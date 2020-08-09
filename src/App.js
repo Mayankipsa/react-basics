b79116b27db63becc962a72b26f2c8bf1d9be4ef
Mayank <mayankipsa@gmail.com>
2020-08-09 19:31:36 +0530

    Removed HardCoding from Table and updates frm form

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from "./FormAddChar";
 
 class App extends Component {
-  
+
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
+    characters: []
   }
 
   removeCharacter = (index) => {
-    const {characters} = this.state
-  
+    const { characters } = this.state
+
     this.setState({
       characters: characters.filter((character, i) => {
         return i !== index
@@ -34,14 +18,22 @@ class App extends Component {
     })
   }
 
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+  submitForm = () => {
+    this.props.handleSubmit(this.state)
+    this.setState(this.initialState)
+  }
   render() {
 
-    const {characters} = this.state
+    const { characters } = this.state
 
     return (
       <div className="container">
         <h1>Hello, React Again!</h1>
         <Table charactersData={characters} removeCharacter={this.removeCharacter}></Table>
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     )
   }
