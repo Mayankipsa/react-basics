6cda41f8e2b34f71871922468d58a047f0d44982
Mayank <mayankipsa@gmail.com>
2020-08-09 15:48:29 +0530

    Passed Data to Table using Props

--- src/Table.js
+++ src/Table.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 
 class Table extends Component {
     render() {
+        const {charactersData}=this.props
+
         return (
             <table className='table'>
                 <TableHeader></TableHeader>
-                <TableBody></TableBody>
+                <TableBody charactersData={charactersData}></TableBody>
             </table>
         )
     }
@@ -23,25 +25,20 @@ const TableHeader = () => {
     )
 }
 
-const TableBody = () => {
+const TableBody = (props) => {
+
+    const rows = props.charactersData.map((row, index) => {
+        return (
+          <tr key={index}>
+            <td>{row.name}</td>
+            <td>{row.job}</td>
+          </tr>
+        )
+      })
+
     return (
         <tbody>
-            <tr>
-                <td>Charlie</td>
-                <td>Janitor</td>
-            </tr>
-            <tr>
-                <td>Mac</td>
-                <td>Bouncer</td>
-            </tr>
-            <tr>
-                <td>Dee</td>
-                <td>Aspiring actress</td>
-            </tr>
-            <tr>
-                <td>Dennis</td>
-                <td>Bartender</td>
-            </tr>
+            {rows}
         </tbody>
     )
 }
